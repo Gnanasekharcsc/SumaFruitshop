7e11e99b1afd7e33aa30bd45ae1da53e2934f373
Gnana Sekhar <gnanasekhark996@gmail.com>
2020-04-05 13:50:00 -0400

    images added

--- src/Home/homepage.jsx
+++ src/Home/homepage.jsx
@@ -1,57 +1,105 @@
 import React from 'react';
 import './homepage.style.scss'
 const HomePage=()=>(
+
 <div className='homepage'>
 <div className='directory-menu'>
 <div className='menu-item'>
 <div className='content'>
-    <h2 className='content'>Apples</h2>
-    <span className='subtitles'>Shop Now</span>
+    <img src={require('./staticimages/bunchbanana.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>Bananas</h2>
+    <button className='buttons'type="button">Add to Cart</button>
 </div>
 </div>
 
 <div className='menu-item'>
 <div className='content'>
-    <h2 className='content'>Mangoes</h2>
-    <span className='subtitles'>Shop Now</span>
+    <img src={require('./staticimages/grapes.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>Black Grapes</h2>
+    <button className='buttons'type="button">Add to Cart</button>
 </div>
 </div>
 
 <div className='menu-item'>
 <div className='content'>
-    <h2 className='content'>Bananas</h2>
-    <span className='subtitles'>Shop Now</span>
+    <img src={require('./staticimages/guava-fruit.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>Guavas</h2>
+    <button className='buttons'type="button">Add to Cart</button>
 </div>
 </div>
 
 <div className='menu-item'>
 <div className='content'>
-
-    <h2 className='content'>Oranges</h2>
-    <span className='subtitles'>Shop Now</span>
+    <img src={require('./staticimages/kiwifruits.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>Kiwi Fruits</h2>
+    <button className='buttons'type="button">Add to Cart</button>
 </div>
 </div>
 
 <div className='menu-item'>
 <div className='content'>
-    <h2 className='content'>Pineapples</h2>
-    <span className='subtitles'>Shop Now</span>
+    <img src={require('./staticimages/lemons.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>Lemons</h2>
+    <button className='buttons'type="button">Add to Cart</button>
 </div>
 </div>
 
 <div className='menu-item'>
 <div className='content'>
-    <h2 className='content'>Mangoes</h2>
-    <span className='subtitles'>Shop Now</span>
+    <img src={require('./staticimages/mangoes.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>Mangeos</h2>
+    <button className='buttons'type="button">Add to Cart</button>
 </div>
 </div>
 
 <div className='menu-item'>
 <div className='content'>
-    <h2 className='content'>Grapes</h2>
-    <span className='subtitles'>Shop Now</span>
+    <img src={require('./staticimages/pomegranate.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>pomegranates</h2>
+    <button className='buttons'type="button">Add to Cart</button>
 </div>
 </div>
+
+<div className='menu-item'>
+<div className='content'>
+    <img src={require('./staticimages/ragus.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>Ragus</h2>
+    <button className='buttons'type="button">Add to Cart</button>
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+    <img src={require('./staticimages/sapota.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>Sapotoes</h2>
+    <button className='buttons'type="button">Add to Cart</button>
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+    <img src={require('./staticimages/sweetApples.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>Sweet Apples</h2>
+    <button className='buttons'type="button">Add to Cart</button>
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+    <img src={require('./staticimages/Watermelon.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>Watermelon</h2>
+    <button className='buttons'type="button">Add to Cart</button>
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+    <img src={require('./staticimages/Apples.jpg')} alt ='banana' className='image'></img>
+    <h2 className='content-Name'>Apples</h2>
+    <button className='buttons'type="button">Add to Cart</button>
+</div>
+</div>
+
 </div>
 </div>
 )
