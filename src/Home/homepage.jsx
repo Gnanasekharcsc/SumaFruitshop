70f3a6873adddd99e3f6530a5a8a938c9df84ad4
Gnana Sekhar <gnanasekhark996@gmail.com>
2020-04-04 22:24:02 -0400

    Base code committed

---
+++ src/Home/homepage.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './homepage.style.scss'
+const HomePage=()=>(
+<div className='homepage'>
+<div className='directory-menu'>
+<div className='menu-item'>
+<div className='content'>
+    <h2 className='content'>Apples</h2>
+    <span className='subtitles'>Shop Now</span>
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+    <h2 className='content'>Mangoes</h2>
+    <span className='subtitles'>Shop Now</span>
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+    <h2 className='content'>Bananas</h2>
+    <span className='subtitles'>Shop Now</span>
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+
+    <h2 className='content'>Oranges</h2>
+    <span className='subtitles'>Shop Now</span>
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+    <h2 className='content'>Pineapples</h2>
+    <span className='subtitles'>Shop Now</span>
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+    <h2 className='content'>Mangoes</h2>
+    <span className='subtitles'>Shop Now</span>
+</div>
+</div>
+
+<div className='menu-item'>
+<div className='content'>
+    <h2 className='content'>Grapes</h2>
+    <span className='subtitles'>Shop Now</span>
+</div>
+</div>
+</div>
+</div>
+)
+
+export default HomePage;
